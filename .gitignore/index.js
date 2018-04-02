72d799ea9a0285f4aabe97cce012c1c7fa1924c2
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-02 16:33:41 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -121,15 +121,15 @@ bot.on('message', message => {
         message.channel.sendEmbed(embed2);
     }
     
-    if (message.content.includes("xD") || message.content.includes("lol") || message.content.includes("mdr")) {
-    	var sayings2 = ["jui mort X)",
-											"ptdr",
-											"C'est pas drôle",
-		       									"C'est pas ouf en faites, jcp pourquoi tu trouve ca drôle",
-											"Ahah cé drol",
-		       									"jui mort x)"];
+    if (message.content.includes("xD")) {
+    	var sayings = ["jui mort X)",
+										"ptdr",
+										"C'est pas drôle",
+		       								"C'est pas ouf en faites, jcp pourquoi tu trouve ca drôle",
+										"Ahah cé drol",
+		       								"jui mort x)"];
 
-			var result2 = Math.floor((Math.random() * sayings2.length) + 0);
-			message.channel.sendMessage(sayings2[result2]);
+			var result = Math.floor((Math.random() * sayings.length) + 0);
+			message.channel.sendMessage(sayings[result]);
     }
 });
