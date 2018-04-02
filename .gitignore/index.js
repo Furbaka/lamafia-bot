c441d199abeb12d9c886fa8da4ef71b8a20c201d
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-02 21:58:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -57,11 +57,11 @@ bot.on('message', message => {
         message.channel.sendMessage("Follow me ma brudda, I know da wae !\n https://www.google.com/maps/place/Uganda/@1.3671051,30.058145,7z/data=!3m1!4b1!4m5!3m4!1s0x1771a69f6499f945:0x874155ce43014549!8m2!3d1.373333!4d32.290275");
     }
 
-    if (message.includes ("pates") || message.content == "burger" || message.content == "pizza" ||  message.content == "pâtes"){
+    if (message.includes("PATES") || message.includes("BURGER") || message.includes("PIZZA") ||  message.contentincludes("PÂTES")){
         message.reply("Arrête de parler de bouffe j'ai faim :/");
     }
     
-    if (message.content == "DAB" || message.content == "dab"){
+    if (message.content.includes("DAB")){
         message.channel.sendMessage("LE DAB NÉ PAS MORT");
     }
 
@@ -69,10 +69,10 @@ bot.on('message', message => {
         message.channel.sendMessage("Donc... je suis pas un vrai petit garcon ? :/");
     }
     
-    if (message.content == "Quelle heure il est ?" || message.content == "quelle heure il est ?" || message.content == "Quelle heure il est " ||message.content == "quelle heure il est ?" || message.content == "QUELLE HEURE IL EST ?" || message.content.startsWith == "il est quelle heure") {
+    if (message.content.includes("HEURE")) {
         message.channel.sendMessage("L'HEURE DE T'ACHETER UNE MONTRE !");
     }
-    if (message.content == "WAW" || message.content == "wow" || message.content == "woaw" || message.content == "WOW" || message.content == "waw"){
+    if (message.content.includes("WOW") || message.content.includes("WAW")){
         message.reply("T'as vu, c'est dingue !");
     }
 
@@ -117,7 +117,7 @@ bot.on('message', message => {
 		.setDescription("AJOUTER LE BOT A VOTRE SERVEUR !")
 		.addField("Lien", "[Ajoute moi !](https://lightlifefr.wix.com/lightbot)", true)
 		.setColor("0xF4D14F")
-		.setFooter("BOT CRÉÉ PAR MISTIGRIX")
+		.setFooter("BOT CRÉÉ PAR <@328514801124900866>")
         message.channel.sendEmbed(embed2);
     }
 
