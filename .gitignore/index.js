9c3217436acd02223a7c376ac3cf575a62919484
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-04 17:46:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -134,22 +134,20 @@ bot.on('message', message => {
     }
 	
     if (message.content.startWith(prefix + "sondage")) {
-        if(message.author.id == "328514801124900866"){
-            let args = message.content.split(" ").slice(1);
-            let thingToEcho = args.join(" ");
-            var embed = new Discord.RichEmbed()
-                .setDescription("Sondage")
-                .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
-                .setColor("0xB40404")
-                .setTimestamp();
-            message.channel.sendEmbed(embed);
-            .then(function (message) {
-            message.react("✔");
-            message.react("❌");
-        }).catch(function() {
-	});
-        }else{
-            return message.reply("Tu n'as pas la persmission.");
+       let args = message.content.split(" ").slice(1);
+       let thingToEcho = args.join(" ");
+       var embed = new Discord.RichEmbed()
+           .setDescription("Sondage")
+           .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+           .setColor("0xB40404")
+           .setTimestamp();
+       message.channel.sendEmbed(embed);
+       .then(function (message) {
+       message.react("✔");
+       message.react("❌");
+    }).catch(function() {
+    }else{
+    return message.reply("Tu n'as pas la persmission.");
 }
-	    
+
 });
