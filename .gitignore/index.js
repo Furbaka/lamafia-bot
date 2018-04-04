77587778a792cb9adb08805d83e8f12135d7bb10
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-04 17:57:59 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -146,7 +146,7 @@ bot.on('message', message => {
        message.react("✔");
        message.react("❌");
     }).catch(function() {
-    });
+    ));
     }else{
     return message.reply("Tu n'as pas la persmission.");
 }
