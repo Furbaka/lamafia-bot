525117b000882b6f069ea1ea4a8b8d7a3c715f1f
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-08 17:03:31 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -49,7 +49,7 @@ bot.on('message', message => {
 
     if (message.content === prefix + "info"){
         var embed = new Discord.RichEmbed()
-		.setThumbnail(bot.user.avatarURL)
+		.setThumbnail(message.guild.iconURL)
 		.setTitle("INFO")
 		.setDescription("Information du serveur")
 		.addField("Nom", message.guild.name)
