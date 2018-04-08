dae8affb25061ae3079332456d8c9bc43bc0f2d4
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-08 18:10:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -257,5 +257,5 @@ bot.on('message', message => {
               request.get(options, (error, response, body) => {
                     var file = JSON.parse(body)
                     message.channel.sendFiles(file.file)
-	      }
+	      )}
 			  })};
