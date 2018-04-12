960f72131eba9a015704661cec0dc20aad3c8222
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-12 17:12:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -291,3 +291,9 @@ bot.on('message', message => {
 	}
 	}
 });
+
+bot.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.emoji.name === ":white_check_mark:") {
+    reaction.message.mentions.members.first.send("kuku");
+    }
+});
