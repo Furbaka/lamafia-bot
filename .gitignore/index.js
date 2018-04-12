2f00bae167c39eb5894b519efdaace638bead3a7
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-12 17:35:50 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -295,7 +295,7 @@ bot.on('message', message => {
 bot.on('messageReactionAdd', (reaction, user) => {
     if(reaction.emoji.name === "✅") {
 	var author = reaction.message.mentions.members.first();
-	var reactor = reaction.user.id
+	var reactor = reaction.user
 	author.sendMessage( "<@" + reactor + ">" + " est partant pour une partie avec toi !");
     }
 });
