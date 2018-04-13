ee27fc8b28a03135c55f132641043163498672ed
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-13 09:04:57 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,7 +5,7 @@ const request = require('request');
 var prefix = ("*");
 
 bot.on('ready', () => {
-	bot.user.setGame(bot.guilds.size + ` serveurs | *aide`);
+	bot.user.setGame(bot.guilds.size + " serveurs | *aide");
 	bot.user.setUsername("⚡Rainbow+");
 });
 
