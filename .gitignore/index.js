b78d3c14a3ca208bc5394415668fb3bacf3c964f
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-04-17 17:03:55 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -348,12 +348,12 @@ var multi = bot.emojis.get("434047602602803200");
     }
 	
     if (message.content === prefix + "aide nsfw"){
-        var embed2 = new Discord.RichEmbed()
+        var embed6 = new Discord.RichEmbed()
 		.setThumbnail("http://holly.paheal.net/_images/10f3ed7efc2c3e737b34d755870197b4/2554566%20-%20ELZZombie%20Friendship_is_Magic%20My_Little_Pony%20Rainbow_Dash.png")
 		.setTitle("A VENIR")
 		.setDescription("A BIENTOT MES GROS COCHONS !")
 		.setColor("cc55ee")
 		.setFooter("BOT CRÉÉ PAR MISTIGRIX, le trop gros cochon !")
-        message.channel.sendEmbed(embed2);
+        message.channel.sendEmbed(embed6);
     }
 });
